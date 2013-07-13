36190deb6196c90d05cfef60c31b3c0256691afc
Shizheng Li <isulsz1@gmail.com>
2013-07-13 12:57:27 -0400

    Add simulation configs for KVChan

--- include/doxygenMain.h
+++ include/doxygenMain.h
@@ -1,43 +1,40 @@
 /** \mainpage Multithreaded Monte Carlo Simulation platform. (MTMCSim)
 *
-*	\section author Author and contact information
-*	Author: Shizheng Li, Dept. of Electrical and Computer Engineering, Iowa State University
+*   \section author Author and contact information
+*   Author: Shizheng Li
+*   This project was done when he was a PhD student at Dept. of Electrical and Computer Engineering, Iowa State University
 *
-*	Contact: szli AT iastate.edu
+*   Contact: szli AT iastate.edu
 *
-*	Important note: The current documentation contains information on unpublished research work.
-*	This documentation is not availbe to general public. Please do not disclose  
-*	this documentation to other people.
 *
-*	All rights reserved. Copyright (c) 2010 Shizheng Li
+*   All rights reserved. Copyright (c) 2010 Shizheng Li
 
-(This project may be opensource in the future.)
-*	
-*	\section sys System Requirement
-*	The project is portable between Windows and Linux. Tested under Windows 7 32-bit and 64-bit, Visual Studio 2010 and Fedora 13 with Gcc 4.4.
-*	
-*	The project requires Boost library. Tested under 1.43 and 1.44 version. Boost::thread must be built on the system. 
+*   
+*   \section sys System Requirement
+*   The project is portable between Windows and Linux. Tested under Windows 7 32-bit and 64-bit, Visual Studio 2010 and Fedora 13 with Gcc 4.4.
+*   
+*   The project requires Boost library. Tested under 1.43/1.44/1.47 version. Boost::thread must be built on the system. We use CMake as build tool. This allow cross-platform builds. 
 *
-*	To obtain high performance, the hardware should offer good parallel compute capability. Typically the number of working threads equals to 
-*	the number of CPU cores. Tested under Intel Core Duo T8100, Intel Core i7-950, Intel Core2 6400 and Intel Xeon X5650. 
+*   To obtain high performance, the hardware should offer good parallel compute capability. Typically the number of working threads equals to 
+*   the number of CPU cores. Tested under Intel Core Duo T8100, Intel Core i7-950, Intel Core2 6400 and Intel Xeon X5650. 
 *
-*	
-*	
-*	\section intro Introduction
-*	This project indends to have a generic platform for mulththreaded Monte Carlo simulations. As long as the simulation is written
-*	according to the interface (abstract class MTMCSim::MTMCSimBase), the multithreading will be handled behind the scene. The multithreading
-*	scheduler runs under boss-worker mode. The boss thread generates random number sequences and the worker threads performs
-*	specified simulations. In current version, there is only one random number generator (RNG), thus one boss thread. We may have parallel RNG in the 
-future but this should be done carefully to avoid any possible correlation between RN sequences. 
+*   
+*   
+*   \section intro Introduction
+*   This project indends to have a generic platform for mulththreaded Monte Carlo simulations. As long as the simulation is written
+*   according to the interface (abstract class MTMCSim::MTMCSimBase), the multithreading will be handled behind the scene. The multithreading
+*   scheduler runs under boss-worker mode. The boss thread generates random number sequences and the worker threads performs
+*   specified simulations. In current version, there is only one random number generator (RNG), thus one boss thread. We may have parallel RNG in the 
+future. Therefore, it is best suited for simulations where the RN generation is simple compared to the algorithm run at each instance of the simulation. 
 
 The project is designed in a purely object oriented manner in C++. This makes the project easy to maintain and extend. Almost all pointers 
 in this project are boost::shared_ptr. All containers are from C++ STL. 
 
-	In this project, we call a bunch of data that gives one sample output to be a frame. In Monte Carlo simulation, we simulation 
-	many frames to get many samples and average over them. This terminalogy is used in communication and coding theory. In other fields, the terminalogy could be different.
-*	
-*	This project also provides a friendly user interface. All the inputs are handled through text files and the user can 
-*	change the display and save format easily. 
+    In this project, we call the data that gives one sample output to be a frame. For example, in communications theory, a frame is a data frame to be transmitted and received. In Monte Carlo simulation, we simulation 
+    many frames to get many samples and average over them. This terminalogy is used in communication and coding theory. In other fields, the terminalogy could be different.
+*   
+*   This project also provides a friendly user interface. All the inputs are handled through text files and the user can 
+*   change the display and save format easily. It is not hard to provide a Python interface given the current design. 
 *
 This documentation is brief. Only displays public members of each classes and documentations for some of the members. 
 
@@ -67,34 +64,34 @@ Several classes are defined for each of the specific simulators for their algori
 
 For a complete list of class hierarchy, please see <a href="hierarchy.html">Class Hierarchy</a> .
 
-*	\section ui User Interface
+*   \section ui User Interface
 
 The user interface is written in a pure OO manner and intended to potentially support  graphic user interface in the future.
 \subsection input Input files
-*	The input parameters are given in text files. Typically, there are two input files. One is the top level configuration file,
-*	the file name is MTMCSimconf.txt by default, or is the input paramater of the exetuable file. A typical top configuration file looks like as follows.
+*   The input parameters are given in text files. Typically, there are two input files. One is the top level configuration file,
+*   the file name is MTMCSimconf.txt by default, or is the input paramater of the exetuable file. A typical top configuration file looks like as follows.
 *
 *<em>
-*	simulation name = MulStageSim   % Simulation name tells which simulator to load \n
-*	workingDir=./ \n
-*	profile file name=MuStSimuNew/QARY256_1.txt  % The file name of the simulation configuration..Relative path from working Dir. \n
-*	temp file name=MuStSimuNew/MulStagetempFile.txt % Temporary resultion file name. Relative path from working Dir .\n
-*	format file name=MuStSimuNew/MuStFormat.txt %Output format file name.Relative path from working Dir. \n
-*	queueSize=8	% The size of the buffer queue between boss thread and working thread. \n
-*	nThreads=4 %The number of working threads.\n
-*	multiple run mode = 0	% 1 if automatically run simulatin using different parameters, the simulation should be MTMCSim::MultiRunnable.\n
-*	no simulation = 0 % If this is 1, we do not run the simulation. Only displays the parameters and computed results. \n
+*   simulation name = MulStageSim   % Simulation name tells which simulator to load \n
+*   workingDir=./ \n
+*   profile file name=MuStSimuNew/QARY256_1.txt  % The file name of the simulation configuration..Relative path from working Dir. \n
+*   temp file name=MuStSimuNew/MulStagetempFile.txt % Temporary resultion file name. Relative path from working Dir .\n
+*   format file name=MuStSimuNew/MuStFormat.txt %Output format file name.Relative path from working Dir. \n
+*   queueSize=8 % The size of the buffer queue between boss thread and working thread. \n
+*   nThreads=4 %The number of working threads.\n
+*   multiple run mode = 0   % 1 if automatically run simulatin using different parameters, the simulation should be MTMCSim::MultiRunnable.\n
+*   no simulation = 0 % If this is 1, we do not run the simulation. Only displays the parameters and computed results. \n
 *</em>
 
 
-*	In general, the input files in this project have the form like above. Each effecive line has the format Parameter name = Parameter value,
-*	and the space or tab before and after the "=" will be ignored. Any line without "=" will be comment line. In an effective line, any string after % 
-*	will be comments. 
+*   In general, the input files in this project have the form like above. Each effecive line has the format Parameter name = Parameter value,
+*   and the space or tab before and after the "=" will be ignored. Any line without "=" will be comment line. In an effective line, any string after % 
+*   will be comments. 
 *
-*	Another input file gives detailed configuration of a specific simulation. Its file name is specified by "profile file name". 
-*	Here is an example.
+*   Another input file gives detailed configuration of a specific simulation. Its file name is specified by "profile file name". 
+*   Here is an example.
 *<em>
-*	QARY symmetric correlation. KV for Distributed source coding. 
+*   QARY symmetric correlation. KV for Distributed source coding. 
 
 alphabet size = 256 \n
 code length = 255\n
@@ -137,13 +134,13 @@ The computed results for a specific simulator are saved in a nested class of the
 virtual function MTMCSim::CompRes::setMapping() to convert the results in data members to std::map<string,string>. MTMCSim::Util::getFormattedStr() will convert the parameters and 
 computed results into string using given format.
 
-*	\section howto How to write a simulation
+*   \section howto How to write a simulation
 *
-*	To write a specified simulation, a simulator class should be derived from  MTMCSim::MTMCSimBase class. See the doc of MTMCSim::MTMCSimBase
-*	for more information. 
+*   To write a specified simulation, a simulator class should be derived from  MTMCSim::MTMCSimBase class. See the doc of MTMCSim::MTMCSimBase
+*   for more information. 
 
 A good example of such a simulator  with detailed comments is MTMCSim::MulStageSim.
 
-*	
-*	
+*   
+*   
 */
